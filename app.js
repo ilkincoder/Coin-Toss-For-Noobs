4b0fc1bd767d743a36914ad5b2cdd581236ddfeb
İlkin <75911670+ilkincoder@users.noreply.github.com>
2024-02-07 23:30:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+// Getting DOM elements
+
 const coinIcon = document.getElementById('coin');
 const tossBtn = document.getElementById('toss-button');
 const result = document.querySelector('.result');
+
+// Adding callbacks
 coinIcon.insertAdjacentElement('afterend', result);
 tossBtn.addEventListener('click', () => {
   tossBtn.disabled = true
   tossCoinFunction();
 })
 
+
+// Function to identify coin toss 
 function tossCoinFunction() {
   const randomVal = Math.random();
   const faceCoin = randomVal < 0.5 ? 'Heads' : 'Tails';
